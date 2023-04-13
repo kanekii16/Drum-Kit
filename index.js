7880b3565aa7d0d4d9f9e44bd48b055306895c55
kanekii16 <iamsai1616@gmail.com>
2023-04-13 11:24:57 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,57 @@
+
+//Detecting Button Press
+var noOfDbuttons = document.querySelectorAll(".drum").length;
+
+
+for (var i = 0; i<noOfDbuttons; i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+   var buttonInnerHTML= this.innerHTML;
+   makeSound(buttonInnerHTML);
+});
+}
+
+//Detecting Keyboard
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+
+  switch (key) {
+
+    case 'w':
+    var tom1 = new Audio("sounds/tom-1.mp3");
+    tom1.play();
+    break;
+    case 'a':
+    var tom2 = new Audio("sounds/tom-2.mp3");
+    tom2.play();
+    break;
+    case 's':
+    var tom3 = new Audio("sounds/tom-3.mp3");
+    tom3.play();
+    break;
+    case 'd':
+    var tom4 = new Audio("sounds/tom-4.mp3");
+    tom4.play();
+    break;
+    case 'j':
+    var kickbass = new Audio("sounds/kick-bass.mp3");
+    kickbass.play();
+    break;
+    case 'k':
+    var snare = new Audio("sounds/snare.mp3");
+    snare.play();
+    break;
+
+    case 'l':
+    var crash = new Audio("sounds/crash.mp3");
+    crash.play();
+    break;
+    default:
+
+  }
+
+}
